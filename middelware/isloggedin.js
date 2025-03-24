d13c32d58fdb9851128defd25979f5424ae5b444
Sachin Bajaj <bajajsachin101@gmail.com>
2025-03-25 00:36:54 +0530

    .........

--- middelware/isloggedin.js
+++ middelware/isloggedin.js
@@ -4,9 +4,7 @@ const Buyer = require("../mongoose/Buyer");
 module.exports = async function (req, res, next) {
     try {
         if (!req.cookies.userSession) {
-            console.log("No token found");
-            return res.status(401).json({ message: "Unauthorized: No token provided" });
-            res.redirect("/Sign-up");
+            res.redirect("/SignUp");
         }
 
         let decoded = jwt.verify(req.cookies.userSession, "u34yti3yv7ey4v84tv78yrf7y4vt48");
@@ -20,6 +18,6 @@ module.exports = async function (req, res, next) {
         req.user = buyer;
         next();
     } catch (error) {
-        return res.redirect("/")
+        return res.redirect("/SignUp")
     }
 };
